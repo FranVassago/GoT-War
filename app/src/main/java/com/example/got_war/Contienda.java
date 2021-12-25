560d87e525736b0e0c4a4630e09c95a1853b3856
FranVassago <95682928+FranVassago@users.noreply.github.com>
2021-12-25 16:43:52 +0100

    update Contienda.java

--- app/src/main/java/com/example/got_war/Contienda.java
+++ app/src/main/java/com/example/got_war/Contienda.java
@@ -17,171 +17,171 @@ public class Contienda {
     private ArrayList<Accion> acciones;
     private ArrayList<Personaje> personajes;
 
-    public Contienda(Activity context, Jugador jugador1, Jugador jugador2, String fase) {
-        this.context = context;
-        this.jugador1 = jugador1;
-        this.jugador2 = jugador2;
-        this.fase = fase;
-        this.turno = 0;
-
-        this.acciones = new ArrayList<Accion>();
-        this.personajes = new ArrayList<Personaje>();
-    }
-
-    public String getFase () { return fase; }
-
-    public void setFase (String fase) {
-        this.fase = fase;
-
-        TextView tvFase;
-        tvFase = context.findViewById(R.id.tvFase);
-        tvFase.setText(personajes.get(turno).getNombre() + ": " + fase);
-    }
-
-    public Personaje pSeleccionado () {
-        return this.personajes.get(turno);
-    }
-
-    public void iniciarContienda () {
-
-        establecerTurnos(jugador1, jugador2);
-        seleccionarPersonaje(personajes.get(turno));
-
-    }
-
-    public void declararAccion (String habilidad) {
-
-        setFase("DECLARACION");
-
-        declaracion.setEjecutor(pSeleccionado());
-        declaracion.setHabilidad(habilidad);
-
-        if (habilidad == "DEFENDER") {
-           declaracion.setPrioridad((float) 100 + pSeleccionado().getVoluntad() / 100);
-        } else {
-           declaracion.setPrioridad(pSeleccionado().getIniciativa());
+    //Constructor
+        public Contienda(Activity context, Jugador jugador1, Jugador jugador2, String fase) {
+            this.context = context;
+            this.jugador1 = jugador1;
+            this.jugador2 = jugador2;
+            this.fase = fase;
+            this.turno = 0;
+    
+            this.acciones = new ArrayList<Accion>();
+            this.personajes = new ArrayList<Personaje>();
         }
 
-        if (habilidad == "ATACAR") {
-           setFase("SELECCIONAR.OBJETIVO");
-           //iniciarModoSeleccion();
-        } else {
-           acciones.add(declaracion);
-           siguienteTurno();
+    //Getters and Setters
+        public String getFase () { return this.fase; }
+    
+        public void setFase (String fase) { this.fase = fase; }
+
+    //Metodos Gestion Elementos Visuales
+        public void actualizarTvFase ()
+        {
+            TextView tvFase = context.findViewById(R.id.tvFase);
+            tvFase.setText(pSeleccionado.getNombre() + ": " + this.fase);
         }
-    }
-
-    public void seleccionarObjetivo(Jugador jugador, Integer posicion) {
-        declaracion.setVictimas(jugador.getEquipo().get(posicion));
-        acciones.add(declaracion);
-        siguienteTurno();
-    }
-
-    public void establecerTurnos(Jugador jugador1, Jugador jugador2) {
-
-        personajes.clear();
-
-        //Carga los personajes de los jugadores en la lista de turnos
-        for (Personaje personaje : jugador1.getEquipo()) { personajes.add(personaje); }
-        for (Personaje personaje : jugador2.getEquipo()) { personajes.add(personaje);}
-
-        //Se ordenan los personajes por orden de iniciativa / voluntad
-        Boolean reordenado = true;
-        Personaje personajeAux = null;
-
-        while (reordenado) {
-            reordenado = false;
-            for (int i = 0; i < personajes.size(); i++) {
-                if (i > 0)
-                    if (personajes.get(i - 1).getIniciativa() > personajes.get(i).getIniciativa()) {
-                        personajeAux = personajes.get(i - 1);
-                        personajes.set(i - 1, personajes.get(i));
-                        personajes.set(i, personajeAux);
-                        reordenado = true;
-                    }
+    
+    //Metodos de Gestión del Combate
+        public Personaje pSeleccionado () {
+            return this.personajes.get(turno);
+        }
+    
+        public void iniciarRonda () {
+    
+            establecerTurnos(jugador1, jugador2);
+            actualizarModificadores(personajes);
+            seleccionarPersonaje(personajes.get(turno));
+    
+        }
+    
+        public void declararAccion (String habilidad) {
+    
+            setFase("DECLARACION");
+    
+            declaracion.setEjecutor(pSeleccionado());
+            declaracion.setHabilidad(habilidad);
+    
+            if (habilidad == "DEFENDER") {
+               declaracion.setPrioridad((float) 100 + pSeleccionado().getVoluntad() / 100);
+            } else {
+               declaracion.setPrioridad(pSeleccionado().getIniciativa());
+            }
+    
+            if (habilidad == "ATACAR") {
+               setFase("SELECCIONAR.OBJETIVO");
+               //iniciarModoSeleccion();
+            } else {
+               acciones.add(declaracion);
+               siguienteTurno();
             }
         }
-
-    }
-
-    public void siguienteTurno() {
-
-        turno++;
-        if (turno >= personajes.size()){
-            ejecutarAcciones();
-            establecerTurnos(jugador1, jugador2);
-            acciones.clear();
-            turno = 0;
-        }
-
-        seleccionarPersonaje(personajes.get(turno));
-    }
-
-    public void seleccionarPersonaje (Personaje personaje) {
-
-        declaracion = new Accion();
-
-        if (getJugador(personaje).esIA())
-        {
-            declaracion.setPrioridad(pSeleccionado().getIniciativa());
-            declaracion.setEjecutor(personaje);
-            declaracion.setHabilidad("ATACAR");
-            declaracion.setVictimas(jugador1.getEquipo().get(0));
+    
+        public void seleccionarObjetivo(Jugador jugador, Integer posicion) {
+            declaracion.setVictimas(jugador.getEquipo().get(posicion));
             acciones.add(declaracion);
             siguienteTurno();
-        } else {
-            //mostrar habilidades
-            setFase("SELECCIONAR.ACCION");
         }
-
-    }
-
-    public Jugador getJugador (Personaje personaje) {
-        if (personaje.getJugador() == 1)
-            return jugador1;
-        else
-            return jugador2;
-    }
-
-
-    public void ejecutarAcciones (){
-
-        //Se ordenan las acciones por orden de prioridad
-        Boolean reordenado = true;
-        Accion accionAux = null;
-
-        while (reordenado) {
-            reordenado = false;
-            for (int i = 0; i < acciones.size(); i++) {
-                if (i > 0) {
-                    if (acciones.get(i - 1).getPrioridad() > acciones.get(i).getPrioridad()) {
-                        accionAux = acciones.get(i - 1);
-                        acciones.set(i - 1, acciones.get(i));
-                        acciones.set(i, accionAux);
-                        reordenado = true;
-                    }
+    
+        public void establecerTurnos(Jugador jugador1, Jugador jugador2) {
+    
+            personajes.clear();
+    
+            //Carga los personajes de los jugadores en la lista de turnos
+            for (Personaje personaje : jugador1.getEquipo()) { personajes.add(personaje); }
+            for (Personaje personaje : jugador2.getEquipo()) { personajes.add(personaje); }
+    
+            //Se ordenan los personajes por orden de iniciativa / voluntad
+            Boolean reordenado = true;
+            Personaje personajeAux = null;
+    
+            while (reordenado) {
+                reordenado = false;
+                for (int i = 0; i < personajes.size(); i++) {
+                    if (i > 0)
+                        if (personajes.get(i - 1).getIniciativa() > personajes.get(i).getIniciativa()) {
+                            personajeAux = personajes.get(i - 1);
+                            personajes.set(i - 1, personajes.get(i));
+                            personajes.set(i, personajeAux);
+                            reordenado = true;
+                        }
                 }
             }
         }
-
-        //Se ejecutan las acciones
-        for (Accion accion : acciones) {
-           switch (accion.getHabilidad()) {
-
-              case "ATACAR":
-                 for (Personaje victima : accion.getVictimas()) {
-                    victima.recibirDanio(accion.getEjecutor().getAtaque(), "FISICO");
-                 }
-                 break;
-
-              case "DEFENDER":
-                 accion.getEjecutor().setDefendiendo(true);
-                 break;
-           }
+    
+        public void siguienteTurno() {
+    
+            turno++;
+            if (turno >= personajes.size()){
+                ejecutarAcciones();
+                establecerTurnos(jugador1, jugador2);
+                acciones.clear();
+                turno = 0;
+            }
+    
+            seleccionarPersonaje(personajes.get(turno));
         }
-
-    }
-
-
+    
+        public void seleccionarPersonaje (Personaje personaje) {
+    
+            declaracion = new Accion();
+    
+            if (getJugador(personaje).esIA())
+            {
+                declaracion.setPrioridad(pSeleccionado().getIniciativa());
+                declaracion.setEjecutor(personaje);
+                declaracion.setHabilidad("ATACAR");
+                declaracion.setVictimas(jugador1.getEquipo().get(0));
+                acciones.add(declaracion);
+                siguienteTurno();
+            } else {
+                //mostrar habilidades
+                setFase("SELECCIONAR.ACCION");
+            }
+        }
+    
+        public Jugador getJugador (Personaje personaje) {
+            if (personaje.getJugador() == 1)
+                return jugador1;
+            else
+                return jugador2;
+        }
+    
+    
+        public void ejecutarAcciones (){
+    
+            //Se ordenan las acciones por orden de prioridad
+            Boolean reordenado = true;
+            Accion accionAux = null;
+    
+            while (reordenado) {
+                reordenado = false;
+                for (int i = 0; i < acciones.size(); i++) {
+                    if (i > 0) {
+                        if (acciones.get(i - 1).getPrioridad() > acciones.get(i).getPrioridad()) {
+                            accionAux = acciones.get(i - 1);
+                            acciones.set(i - 1, acciones.get(i));
+                            acciones.set(i, accionAux);
+                            reordenado = true;
+                        }
+                    }
+                }
+            }
+    
+            //Se ejecutan las acciones
+            for (Accion accion : acciones) {
+               switch (accion.getHabilidad()) {
+    
+                  case "ATACAR":
+                     for (Personaje victima : accion.getVictimas()) {
+                        victima.recibirDanio(accion.getEjecutor().getAtaque(), "FISICO");
+                     }
+                     break;
+    
+                  case "DEFENDER":
+                     accion.getEjecutor().setDefendiendo(true);
+                     break;
+               }
+            }
+        }
 
 }
