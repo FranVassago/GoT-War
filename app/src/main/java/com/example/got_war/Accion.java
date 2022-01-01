58a14adc8968963fdca06215663f86c5b8ff39be
FranVassago <95682928+FranVassago@users.noreply.github.com>
2022-01-01 16:11:23 +0100

    update Accion.java

--- app/src/main/java/com/example/got_war/Accion.java
+++ app/src/main/java/com/example/got_war/Accion.java
@@ -15,53 +15,40 @@ public class Accion {
     }
 
     //Getters
-        public float getPrioridad() {
-        return this.prioridad;
-    }
-        public Habilidad getHabilidad() {
-        return this.habilidad;
-    }
-        public ArrayList<Personaje> getVictimas() {
-        return this.victimas;
-    }
+        public float getPrioridad() { return this.prioridad; }
+        public Habilidad getHabilidad() { return this.habilidad; }
+        public ArrayList<Personaje> getVictimas() { return this.victimas; }
         public int DelayAccion() { return this.delayAccion; }
 
     //Setters
-        public void setPrioridad(float prioridad) {
-        this.prioridad = prioridad;
-    }
-        public void setEjecutor(Personaje ejecutor) {
-        this.ejecutor = ejecutor;
-    }
-        public void setHabilidad(Habilidad habilidad) {
-        this.habilidad = habilidad;
-    }
-        public void setVictimas(Personaje victima) {
-        this.victimas.add(victima);
-    }
+        public void setPrioridad(float prioridad) { this.prioridad = prioridad; }
+        public void setEjecutor(Personaje ejecutor) { this.ejecutor = ejecutor; }
+        public void setHabilidad(Habilidad habilidad) { this.habilidad = habilidad; }
+        public void setVictimas(Personaje victima) { this.victimas.add(victima); }
 
 
     //Métodos con la mecánica de las acciones
-    public long ejecutar(long delayTurnos, ReproductorAnimaciones reproductorAnimaciones) {
-        if (habilidad.modificaEnergia()) {
-            ejecutor.modificaEnergiaRestante(habilidad.getEnergia());
-        }
-
-        if (habilidad.causaDanio()) {
-            for (Personaje victima : victimas) {
-                reproductorAnimaciones.animarHabilidad(habilidad, ejecutor, victima, delayTurnos + delayAccion);
-                victima.recibirDanio(ejecutor.getAtaque(), habilidad.getTipoDanio(), delayTurnos + delayAccion);
+        public long ejecutar(long delayTurnos, ReproductorAnimaciones reproductorAnimaciones) {
+            if (habilidad.modificaEnergia()) {
+                ejecutor.modificaEnergiaRestante(habilidad.getEnergia());
             }
+    
+            if (habilidad.causaDanio()) {
+                for (Personaje victima : victimas) {
+                    reproductorAnimaciones.animarHabilidad(habilidad, ejecutor, victima, delayTurnos + delayAccion);
+                    reproductorAnimaciones.textoFlotante(victima, texto, Color.RED, delayTurnos + delayAccion);
+                    victima.recibirDanio(ejecutor.getAtaque(), habilidad.getTipoDanio(), delayTurnos + delayAccion);
+                }
+            }
+    
+            if (habilidad.activaDefensa()) {
+                ejecutor.setDefendiendo(true);
+            }
+    
+            if (habilidad.getModificador() != null){
+                ejecutor.incluirModificador(habilidad.getModificador());
+            }
+    
+            return delayTurnos + habilidad.getDelay();
         }
-
-        if (habilidad.activaDefensa()) {
-            ejecutor.setDefendiendo(true);
-        }
-
-        if (habilidad.getModificador() != null){
-            ejecutor.incluirModificador(habilidad.getModificador());
-        }
-
-        return delayTurnos + habilidad.getDelay();
-    }
 }
